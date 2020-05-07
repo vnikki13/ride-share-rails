833216efaeb81c65d83e29bf87e469b86c9e0807
SuelyBarreto <suely@barreto.us>
2020-05-06 20:51:12 -0700

    Finished Passenger and Driver controllers and routes

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,61 @@
+class PassengersController < ApplicationController
+
+    # shows list of passengers
+    def index
+      @passengers = Passenger.all.sort
+    end
+  
+    # shows individual passenger details
+    def show
+      @passenger = Passenger.find_by(id: params[:id])
+      redirect_to passengers_path if @passenger.nil?
+    end
+  
+    # creates a form
+    def new 
+      @passenger = Passenger.new
+    end
+    
+    # form submit button calls this
+    def create
+      @passenger = Passenger.new(
+        name: params[:passenger][:name],
+        phone_num: params[:passenger][:phone_num],
+      )
+      if @passenger.save
+        redirect_to passenger_path(@passenger.id)
+      else
+        render :new
+      end
+    end
+    
+    # prepares the passenger data to edit it
+    def edit
+      @passenger = Passenger.find_by(id: params[:id])
+      redirect_to passengers_path if @passenger.nil?
+    end
+  
+    # updates the passenger with the data from the form
+    def update
+      @passenger = Passenger.find_by(id: params[:id])
+      if @passenger.nil?
+        redirect_to passengers_path
+      elsif @passenger.update(
+          name: params[:passenger][:name],
+          phone_num: params[:passenger][:phone_num],
+        )
+        redirect_to passenger_path(@passenger.id)
+      else
+        render :edit
+      end
+    end
+  
+    # deletes a passenger
+    def destroy
+      @passenger = Passenger.find_by(id: params[:id])
+      @passenger.destroy unless @passenger.nil?
+      redirect_to passengers_path
+    end
+  
+
+end
