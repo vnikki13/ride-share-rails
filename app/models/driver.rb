1a342ea4f746dc20bba986983eb152049485bf10
Nikki Vaughan <vnikki13@gmail.com>
2020-05-08 12:28:58 -0700

    Added logic to calculate driver avg rating and total earnings. Details displayed on a drivers show page

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,31 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true
+  validates :vin, uniqueness: true
+
+
+  def all_trips
+    trips_for_driver = self.trips.where(driver_id: self.id)
+  end
+
+  def avg_rating
+    trips_for_driver = all_trips
+    ratings = Array.new
+    trips_for_driver.each { |trip| ratings << trip.rating }
+    if ratings.length == 0
+      return 'No ratings'
+    else
+      return ratings.sum / ratings.length
+    end
+  end
+
+  def total_earnings
+    trips_for_driver = all_trips
+    earnings = Array.new
+    trips_for_driver.each do |trip|
+      earnings << (trip.cost - 1.65) * 0.8
+    end
+     return earnings.sum.round(2)
+  end
 end
