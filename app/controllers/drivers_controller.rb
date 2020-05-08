db9fbe3d8dc98a9b89893bda6fd87aeb32f92ef3
Nikki Vaughan <vnikki13@gmail.com>
2020-05-07 23:09:12 -0700

    Can show all trips for a driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,4 +4,34 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+    @driver.update(driver_params)
+    redirect_to driver_path(@driver.id)
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to root_path
+    else
+      @driver.destroy
+      redirect_to drivers_path
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
+
 end
