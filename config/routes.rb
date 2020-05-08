efb889772eab24e5f44554da80292b7f15e9f438
Nikki Vaughan <vnikki13@gmail.com>
2020-05-07 21:03:26 -0700

    Fixed route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'trip#index'
   resources :trip
-  resources :passenger
-  resources :driver
+  # resources :passenger
+  # resources :driver
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :passengers
   resources :drivers
