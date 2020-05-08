136108a280aeada91ca8d5e98366333508a831ad
SuelyBarreto <suely@barreto.us>
2020-05-07 19:25:23 -0700

    Driver views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -19,6 +19,7 @@ class DriversController < ApplicationController
     # form submit button calls this
     def create
       @driver = Driver.new(
+        id: Driver.maximum(:id).next, # calculate next available id
         name: params[:driver][:name],
         vin: params[:driver][:vin],
         available: params[:driver][:available]
