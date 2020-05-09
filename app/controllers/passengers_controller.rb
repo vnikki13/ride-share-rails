ad8696c6ca1a12c15a713c32c779a778666d11af
SuelyBarreto <suely@barreto.us>
2020-05-08 21:24:29 -0700

    Merged and fixed issues

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,80 +5,68 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all.sort
   end
 
-  # shows individual passenger details
-  def show
-    @passenger = Passenger.find_by(id: params[:id])
-    redirect_to passengers_path if @passenger.nil?
-  end
-
   # creates a form
   def new 
     @passenger = Passenger.new
   end
   
-    # shows individual passenger details
-    def show
-      @passenger = Passenger.find_by(id: params[:id])
-      if @passenger.nil?
-        head :not_found
-        return
-      end
+  # shows individual passenger details
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+      
+  # form submit button calls this
+  def create
+    @passenger = Passenger.new(
+      id: Passenger.maximum(:id) ? Passenger.maximum(:id).next : 1,
+      name: params[:passenger][:name],
+      phone_num: params[:passenger][:phone_num],
+    )
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    else
+      render :new
+    end
+  end
+    
+  # prepares the passenger data to edit it
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
     end
   end
   
-    # creates a form
-    def new 
-      @passenger = Passenger.new
-    end
-    
-    # form submit button calls this
-    def create
-      @passenger = Passenger.new(
-        id: Passenger.maximum(:id) ? Passenger.maximum(:id).next : 1,
+  # updates the passenger with the data from the form
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(
         name: params[:passenger][:name],
         phone_num: params[:passenger][:phone_num],
       )
-      if @passenger.save
-        redirect_to passenger_path(@passenger.id)
-      else
-        render :new
-      end
+      redirect_to passenger_path(@passenger.id)
+    else
+      render :edit
     end
-    
-    # prepares the passenger data to edit it
-    def edit
-      @passenger = Passenger.find_by(id: params[:id])
-      if @passenger.nil?
-        head :not_found
-        return
-      end
-    end
+  end
   
-    # updates the passenger with the data from the form
-    def update
-      @passenger = Passenger.find_by(id: params[:id])
-      if @passenger.nil?
-        head :not_found
-        return
-      elsif @passenger.update(
-          name: params[:passenger][:name],
-          phone_num: params[:passenger][:phone_num],
-        )
-        redirect_to passenger_path(@passenger.id)
-      else
-        render :edit
-      end
+  # deletes a passenger
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
     end
-  
-    # deletes a passenger
-    def destroy
-      @passenger = Passenger.find_by(id: params[:id])
-      if @passenger.nil?
-        head :not_found
-        return
-      end
-      @passenger.destroy
-      redirect_to passengers_path
-    end
+    @passenger.destroy
+    redirect_to passengers_path
+  end
   
 end
