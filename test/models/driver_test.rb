d34335d0eceeac65e6dfd7d4de894bf21fd5bd77
SuelyBarreto <suely@barreto.us>
2020-05-10 16:16:04 -0700

    Finished Driver model tests.

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -14,7 +14,6 @@ describe Driver do
     new_driver.save
     driver = Driver.first
     [:name, :vin, :available].each do |field|
-
       # Assert
       expect(driver).must_respond_to field
     end
@@ -25,9 +24,8 @@ describe Driver do
       # Arrange
       new_driver.save
       new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
-      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
-      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
-​
+      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5)
+      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3)
       # Assert
       expect(new_driver.trips.count).must_equal 2
       new_driver.trips.each do |trip|
@@ -50,32 +48,69 @@ describe Driver do
     it "must have a VIN number" do
       # Arrange
       new_driver.vin = nil
+      new_driver.save
 
       # Assert
       expect(new_driver.valid?).must_equal false
       expect(new_driver.errors.messages).must_include :vin
       expect(new_driver.errors.messages[:vin]).must_equal ["can't be blank"]
     end
+
+    it "must have a unique VIN number" do
+      # Arrange
+      new_driver.save
+      conflicting_driver = Driver.new(name: "Cathy", vin: "123", available: true)
+
+      # Assert
+      expect(conflicting_driver.valid?).must_equal false
+      expect(conflicting_driver.errors.messages).must_include :vin
+      expect(conflicting_driver.errors.messages[:vin]).must_equal ["has already been taken"]
+    end
+
   end
 
   # Tests for methods you create should go here
   describe "custom methods" do
     describe "average rating" do
-      # Your code here
+      it "calculates the average rating" do
+        # Arrange
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
+        # Assert - No trips
+        expect(new_driver.avg_rating).must_be_nil
+        # Act and Assert - 1 trip
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5)
+        expect(new_driver.avg_rating).must_equal 5
+        # Act and Assert - 2 trips
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3)
+        expect(new_driver.avg_rating).must_equal 4
+      end
     end
-
     describe "total earnings" do
+      it "calculates the total earnings" do
+        # Arrange
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
+        # Assert - No trips
+        expect(new_driver.avg_rating).must_be_nil
+        # Act and Assert - 1 trip
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, cost: 200)
+        earnings = (200 - 1.65) * 0.8
+        expect(new_driver.total_earnings).must_be_close_to earnings, 0.01
+        # Act and Assert - 2 trips
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, cost: 100)
+        earnings += (100 - 1.65) * 0.8
+        expect(new_driver.total_earnings).must_be_close_to earnings, 0.01
+      end
+    end
+
+    describe "can go online" do # Method missing from model (Suely)
       # Your code here
     end
 
-    describe "can go online" do
+    describe "can go offline" do # Method missing from model (Suely)
       # Your code here
     end
-
-    describe "can go offline" do
-      # Your code here
-    end
-
-    # You may have additional methods to test
+    
   end
 end
